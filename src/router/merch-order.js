80be406984967d4747603538dba56917a6fa9156
Khalid Mohamud <kmohamud1@outlook.com>
2018-07-30 19:15:45 -0700

    remove console logs and cleanup code

--- src/router/merch-order.js
+++ src/router/merch-order.js
@@ -4,8 +4,6 @@ import {Router} from 'express'
 import {bearerAuth} from '../middleware/parser-auth.js'
 import parserBody from '../middleware/parser-body'
 import Order from '../model/merch-order'
-import createError from 'http-errors'
-import {log} from '../lib'
 
 export default new Router()
 .post('/api/order/:id', bearerAuth, parserBody, (req, res, next) => {
